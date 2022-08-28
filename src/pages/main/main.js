2953671e3fc4841f227c80fa70215a98676a4d55
Max <kolxy@qq.com>
2022-08-27 22:51:06 -0700

    Front page

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -8,10 +8,24 @@ class Main extends Component {
         return (
             <div className="main">
                 <div className="main-header">
-                    <img src={logo} className="logo" alt="logo"/>
+                    <div className="header-left">
+                        <img src={logo} className="logo" alt="logo"/>
+                        <div className="header-title">Angular</div>
+                        <div> ( ͡° ͜ʖ ͡°)</div>
+                    </div>
+                    <div className="header-right">
+                        <div className="header-tab">Help</div>
+                        <div className="header-tab">Content</div>
+                    </div>
                 </div>
                 <div className="main-content">
-                    
+                    <div className="resume-box">{/* Wrapper for typewriter */}
+                        <div className="resume">Resume:</div>
+                        <div className="resume-position">Web dev noob</div>
+                        <div className="typewriter">
+                            - Implemented a typewriter effect using ctrl + c/v
+                        </div>
+                    </div>
                 </div>
             </div>
         )
